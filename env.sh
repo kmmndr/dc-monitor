220dcd1d898f654f7ed51326d84ab3dd25fa624a
Thomas Kienlen <kommander@laposte.net>
2022-09-08 21:32:11 +0200

    Add authentication

--- env.sh
+++ env.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+stage=${1:-default}
+
 cat <<EOF
 COMPOSE_PROJECT_NAME=monitor
 EOF
+
+case $stage in
+	'default')
+		cat <<-EOF
+		MONITOR_LOGIN=admin
+		MONITOR_PASSWORD=admin
+		EOF
+		;;
+esac
--- monitor/entrypoint.sh
+++ monitor/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
 
+export MONITOR_PASSWORD="$(caddy hash-password -algorithm scrypt -plaintext $MONITOR_PASSWORD)"
 
 goreman -exit-on-error -set-ports=false -f /Procfile start
