3c66c33e363eab6e55e6438b8657067361f8eb4e
Thomas Kienlen <kommander@laposte.net>
2022-09-08 21:20:45 +0200

    Add goreman

--- monitor/entrypoint.sh
+++ monitor/entrypoint.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 set -eu
 
-service=${1:-'all'}
 
-echo "Entrypoint (service: '$service')"
-
-case $service in
-  'php')
-    php-fpm7 -F
-    ;;
-  'caddy')
-    caddy run -config /etc/Caddyfile
-    ;;
-  'all')
-    # TODO: Do not run more than one process in a container
-    $0 caddy &
-    $0 php
-    ;;
-  *)
-    exec "$@"
-    ;;
-esac
+goreman -exit-on-error -set-ports=false -f /Procfile start
